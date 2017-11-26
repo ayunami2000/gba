8de928f7c8cf74bd4de24febf09f85e5fcf4724b
ayunami2000 <spwilliamsiam@gmail.com>
2017-11-26 17:59:33 -0500

    Update IodineGBACoreGlueCode.js

--- user_scripts/IodineGBACoreGlueCode.js
+++ user_scripts/IodineGBACoreGlueCode.js
@@ -131,7 +131,7 @@ window.onload = function () {
     //Hook the GUI controls.
     registerGUIEvents();
     //Initialize the audio and enable sound:
-    if(/iPad|iPhone|iPod/.test(navigator.userAgent)&&!window.MSStream){window.ontouchstart=function(){registerAudioHandler();Iodine.enableAudio();this.ontouchstart=function(event){};}}else{registerAudioHandler();Iodine.enableAudio();}
+    if(/iPad|iPhone|iPod/.test(navigator.userAgent)&&!window.MSStream){window.onclick=function(){registerAudioHandler();Iodine.enableAudio();this.onclick=function(event){};}}else{registerAudioHandler();Iodine.enableAudio();}
     //Download the BIOS:
     downloadBIOS();
 }
