9486a472929e5d0c8bf264abbd69752fd3c4dbe1
ayunami2000 <spwilliamsiam@gmail.com>
2017-11-26 16:45:33 -0500

    Update IodineGBACoreGlueCode.js

--- user_scripts/IodineGBACoreGlueCode.js
+++ user_scripts/IodineGBACoreGlueCode.js
@@ -117,8 +117,9 @@ var Mixer = null;
 var MixerInput = null;
 var timerID = null;
 window.onload = function () {
+    if(!location.href.includes("#")){window.location="/gba/";return;}
     if (!games[location.hash.substr(1)]) {
-        alert("Invalid game request!");
+        alert("Error: User defined ROMs are not supported at this time.");
         return;
     }
     //Initialize Iodine:
