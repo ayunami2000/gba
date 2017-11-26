ec845a371cf5c5b2e8efabbcaf1cc649eda43d3d
ayunami2000 <spwilliamsiam@gmail.com>
2017-11-26 12:22:55 -0500

    Update IodineGBAAudioGlueCode.js

--- user_scripts/IodineGBAAudioGlueCode.js
+++ user_scripts/IodineGBAAudioGlueCode.js
@@ -8,6 +8,7 @@
  
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+audioInit();
 function audioInit(){
 if(typeof GlueCodeMixer!=="function"){
 function GlueCodeMixer(){
@@ -250,4 +251,8 @@ AudioSimpleBuffer.prototype.getSlice = function () {
         }
     }
 }
+this.GlueCodeMixer=GlueCodeMixer;
+this.GlueCodeMixerInput=GlueCodeMixerInput;
+this.AudioBufferWrapper=AudioBufferWrapper;
+this.AudioSimpleBuffer=AudioSimpleBuffer;
 }
